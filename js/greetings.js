25df300b360bce3499fe8dc53091f6dcd37dfc06
mnmsgit <102347351+mnmsgit@users.noreply.github.com>
2022-06-03 00:35:38 +0900

    Update greetings.js

--- js/greetings.js
+++ js/greetings.js
@@ -4,14 +4,16 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const firstWindow = document.querySelector("#firstWindow");
 const greeting = document.querySelector("#greeting");
-
+const loginBtn = document.querySelector("#logout-box")
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
+const BLOCK_STYLE = "block";
+const NONE_STYLE = "none";
 
 function onLoginSubmit(event) {
   event.preventDefault();
-  firstWindow.style.display = "none";
+  firstWindow.style.display = NONE_STYLE;
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
   localStorage.setItem(USERNAME_KEY, username);
@@ -23,14 +25,22 @@ function paintGreetings(username) {
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
+function onLogoutSubmit(event){
+  event.preventDefault();
+  localStorage.removeItem(USERNAME_KEY);
+  firstWindow.style.display = BLOCK_STYLE;
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+}
+loginBtn.addEventListener("click",onLogoutSubmit)
+
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 if (savedUsername === null) {
-    firstWindow.style.display = "block";
+    firstWindow.style.display = BLOCK_STYLE;
     loginForm.classList.remove(HIDDEN_CLASSNAME);
     loginForm.addEventListener("submit", onLoginSubmit);
 
 } else {
     paintGreetings(savedUsername);
-    firstWindow.style.display = "none";
+    firstWindow.style.display = NONE_STYLE;
 }
